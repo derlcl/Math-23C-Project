da3754ed033a62c38aac59450cf0c59206c5d4e0
Derl Clausen <derlclausen@derls-mbp.home>
2020-03-23 21:52:42 -0700

    Added Regime Categories

--- Main.R
+++ Main.R
@@ -1,2 +1,20 @@
 #This is where our main code for our project will go!
-#Making sure this worked!
+DJI <- read.csv("DJI.csv"); head(DJI)
+
+#Create 'Regime' Categories (Regan, Bush, etc.)
+DJI["Regime"] <- "None" #Set blank column
+
+#Set the dates for each regime
+#Each 'term' is denoted from inaugaration day 
+#to the day before the next regime's inaugaration day
+dates <- sapply(DJI["Date"], function(x) as.Date(x)) #Convert CSV date character objects to date objects
+DJI[dates < as.Date("1989-01-20"), "Regime"] <- "RR"
+DJI[dates >= as.Date("1989-01-20") & dates < as.Date("1993-01-20"), "Regime"] <- "GHWB"
+DJI[dates >= as.Date("1993-01-20") & dates < as.Date("2001-01-20"), "Regime"] <- "BC"
+DJI[dates >= as.Date("2001-01-20") & dates < as.Date("2009-01-20"), "Regime"] <- "GWB"
+DJI[dates >= as.Date("2009-01-20") & dates < as.Date("2017-01-20"), "Regime"] <- "BO"
+DJI[dates >= as.Date("2017-01-20"), "Regime"] <- "DJT"
+
+
+
+
