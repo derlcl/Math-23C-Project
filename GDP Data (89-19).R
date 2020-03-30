b58e29731fdff9ce515803aebd859b7a9afa3b6d
brunokomel <brunokomel@gmail.com>
2020-03-30 17:37:36 -0300

    Edited so we had data on recessions by quarter

--- GDP Data (89-19).R
+++ GDP Data (89-19).R
@@ -1,26 +1,24 @@
 #Note that we got this dataset from the FRED website, and this is real GDP growth (which had larger fluxes)
-#Note 2: I did this based on year over year fluxes, but the .csv file has data by quarter
+#Note 2: I did this based on quarter over quarter fluxes.
 GDPb <- read.csv("GDPC1.csv"); head(GDPb) 
 
-BOY <- subset(GDPb, format.Date(GDPb$DATE, "%m") == "01"); head(BOY) #selecting only dates for beginning of year
-BOY$DATE <- as.Date(BOY$DATE); BOY <- data.frame(BOY) #formating dates and the data
-N <- length(BOY$GDPC1)
-GDPg <- numeric(N) #now creating a vector to store the flux in GDP for each year
+N <- length(GDPb$GDPC1)
+GDPg <- numeric(N) #now creating a vector to store the flux in GDP for each quarter
 for(i in 2:N){ 
   #since we're not interested the data until 1989, we can start from the second row
-  GDPg[i] <- BOY$GDPC1[i] - BOY$GDPC1[i-1]
+  GDPg[i] <- GDPb$GDPC1[i] - GDPb$GDPC1[i-1]
 }
-BOY <- data.frame(BOY,GDPg) 
+Recess <- numeric(N)
+for(i in 1:N){
+  Recess[i] <- GDPg[i] < 0
+}
 
-GOM <- numeric(length(BOY$GDPg)) #GOM here stands for Growth Over Mean
-for(i in 1:length(BOY$GDPg)){
-  GOM[i] <- BOY$GDPg[i] > mean(GDPg) #using mean GDP growth here because 
-  #there have been very few YOY recessions, so this will tell us
-  #whether the growth in a given year was better than the average year from 1947-2019)
-}
-GDPf <- data.frame(BOY,GOM); head(GDPf)
+GDPb <- data.frame(GDPb,GDPg, Recess) ; GDPb$DATE <- as.Date(GDPb$DATE); head(GDPb)
+
 
 #selecting only the relevant years for our analysis
-GDP <- subset(GDPf, DATE >= "1989-01-01" & DATE <= "2019-01-01"); head(GDP) 
+GDP <- subset(GDPb, DATE >= "1989-01-01" & DATE <= "2019-01-01"); head(GDP) 
+sum(GDP$Recess) #So there have been 12 quarters with recessions
+
 
 
