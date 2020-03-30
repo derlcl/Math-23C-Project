79794bd04f2fe9d5c42010013c39b11887abf3c4
rakeentanvir <raktanbc@gmail.com>
2020-03-29 20:33:00 -0400

    Fixed some possible typos in GDP Data R Script. Added indices and Open value differences to set up for permutation test and eventual random walk modeling.

--- GDP Data (89-19).R
+++ GDP Data (89-19).R
@@ -1,8 +1,8 @@
 #Note that we got this dataset from the FRED website, and this is real GDP growth (which had larger fluxes)
 #Note 2: I did this based on year over year fluxes, but the .csv file has data by quarter
-GDPb <- read.csv("GDPC1.csv"); head(GDP) 
+GDPb <- read.csv("GDPC1.csv"); head(GDPb) 
 
-BOY <- subset(GDPb, format.Date(GDP$DATE, "%m")=="01"); head(BOY) #selecting only dates for beginning of year
+BOY <- subset(GDPb, format.Date(GDPb$DATE, "%m") == "01"); head(BOY) #selecting only dates for beginning of year
 BOY$DATE <- as.Date(BOY$DATE); BOY <- data.frame(BOY) #formating dates and the data
 N <- length(BOY$GDPC1)
 GDPg <- numeric(N) #now creating a vector to store the flux in GDP for each year
@@ -12,7 +12,7 @@ for(i in 2:N){
 }
 BOY <- data.frame(BOY,GDPg) 
 
-GOM <- numeric(length(BOY$GDPg)) #GOA here stands for Growth Over Mean
+GOM <- numeric(length(BOY$GDPg)) #GOM here stands for Growth Over Mean
 for(i in 1:length(BOY$GDPg)){
   GOM[i] <- BOY$GDPg[i] > mean(GDPg) #using mean GDP growth here because 
   #there have been very few YOY recessions, so this will tell us
--- Main.R
+++ Main.R
@@ -7,7 +7,8 @@ DJI["Regime"] <- "None" #Set blank column
 #Set the dates for each regime
 #Each 'term' is denoted from inaugaration day 
 #to the day before the next regime's inaugaration day
-dates <- sapply(DJI["Date"], function(x) as.Date(x)) #Convert CSV date character objects to date objects
+dates <- sapply(DJI["Date"], function(x) as.Date(x)) #Convert CSV date character objects 
+#to date objects
 DJI[dates < as.Date("1989-01-20"), "Regime"] <- "RR"
 DJI[dates >= as.Date("1989-01-20") & dates < as.Date("1993-01-20"), "Regime"] <- "GHWB"
 DJI[dates >= as.Date("1993-01-20") & dates < as.Date("2001-01-20"), "Regime"] <- "BC"
@@ -15,7 +16,32 @@ DJI[dates >= as.Date("2001-01-20") & dates < as.Date("2009-01-20"), "Regime"] <-
 DJI[dates >= as.Date("2009-01-20") & dates < as.Date("2017-01-20"), "Regime"] <- "BO"
 DJI[dates >= as.Date("2017-01-20"), "Regime"] <- "DJT"
 
-#Create a binary category for the political party in control of the White House (Republican = 1, Democrat = 0)
-Republican <- (DJI$Regime == "RR")|(DJI$Regime == "GHWB")|(DJI$Regime=="GWB")|(DJI$Regime=="DJT")
+#Create a binary category for the political party in control of the White House 
+#(Republican = 1, Democrat = 0)
+Republican <- (DJI$Regime == "RR") | (DJI$Regime == "GHWB") | (DJI$Regime == "GWB") | (DJI$Regime == "DJT")
 DJI <- data.frame(DJI, Republican); head(DJI); sum(Republican); mean(Republican)
+
+#Set up indices for permutation test by party and president 
+index.Republican <- DJI$Republican ; sum(index.Republican) # 4822, matches
+index.RR <- (DJI$Regime == "RR") ; sum(index.RR) # 1005
+index.GHWB <- (DJI$Regime == "GHWB") ; sum(index.GHWB) # 1011
+index.BC <- (DJI$Regime == "BC") ; sum(index.BC) # 2021
+index.GWB <- (DJI$Regime == "GWB") ; sum(index.GWB) # 2009
+index.BO <- (DJI$Regime == "BO") ; sum(index.BO) # 2015
+index.DJT <- (DJI$Regime == "DJT") ; sum(index.DJT) # 797
+dim(DJI) ; sum(c(index.RR, index.GHWB, index.BC, index.GWB, index.BO, index.DJT)) # 8858, matches
+
+# Create vector for difference between daily Open values
+Open <- DJI$Open
+N <- length(Open) ; diffs <- numeric(N - 1)
+for (i in 1:N - 1) {
+  diffs[i] <- Open[i + 1] - Open[i]
+}
+head(diffs) ; length(diffs) ; mean(diffs) ; var(diffs) ; sum(diffs) # whopping 13760.49 for variance
+
+diffs.Republican <- diffs[index.Republican[-1]]
+mean(diffs.Republican) # -.006959248
+diffs.Democrat <- diffs[!(index.Republican[-1])]
+mean(diffs.Democrat) # 4.709857
+# The means seem different, but given the large variance, this is doubtful.
 
